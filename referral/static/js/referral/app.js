19d7d3a2bd27d67ecfca94c81bd64a1302a87f2a
David Miller <david@deadpansincerity.com>
2015-05-01 15:57:47 +0100

    We put ngprogress in OPAL core!

--- referral/static/js/referral/app.js
+++ referral/static/js/referral/app.js
@@ -20,23 +20,7 @@ var app = OPAL.module('opal.referral', [
     'opal.referral.controllers'
 ]);
 
-app.run(['$rootScope', 'ngProgressLite', function($rootScope, ngProgressLite) {
-    // When route started to change.
-    $rootScope.$on('$routeChangeStart', function() {
-        ngProgressLite.set(0);
-        ngProgressLite.start();
-    });
-
-    // When route successfully changed.
-    $rootScope.$on('$routeChangeSuccess', function() {
-        ngProgressLite.done();
-    });
-
-    // When some error occured.
-    $rootScope.$on('$routeChangeError', function() {
-        ngProgressLite.set(0);
-    });
-}]);
+OPAL.run(app);
 
 app.config(function($routeProvider){
     $routeProvider.when('/', {redirectTo: '/list'})
