6af940c51c2b3542881c7792831d88a6ed7c5f0d
David Miller <david@deadpansincerity.com>
2015-05-05 15:42:22 +0100

    non stubbed javascript tests!

--- referral/static/js/test/referral.controller.test.js
+++ referral/static/js/test/referral.controller.test.js
@@ -10,7 +10,9 @@ describe('ReferralCtrl', function (){
         $controller  = $injector.get('$controller');
         $httpBackend = $injector.get('$httpBackend');
 
-        referral_route = {};
+        referral_route = {
+            slug: 'test'
+        };
         options = options;
 
         controller = $controller('ReferralCtrl', {
@@ -20,11 +22,32 @@ describe('ReferralCtrl', function (){
             referral_route  : referral_route
         });
     }));
+
     
+    describe('new_patient()', function (){
+        it('Should set $scope.patient', function () {
+            expect($scope.patient).toBe(null);
+            $scope.new_patient()
+            expect($scope.patient.demographics).toEqual([{}]);
+        });
+    });
     
     describe('refer()', function (){
         it('Should hit the api', function () {
-            expect(1).toBe(1);
+            $scope.hospital_number = '1234';
+            
+            $httpBackend.expectPOST('/api/v0.1/referral/test', {
+                hospital_number: '1234',
+                demographics   : {}
+            }).respond({success: 'YAY'});
+
+            $scope.new_patient();
+            $scope.refer();
+            $httpBackend.flush();
+            
+            expect($scope.post_patient_text).toBe(null);
+            expect($scope.state).toBe('success');
+            
         });
     });
     
