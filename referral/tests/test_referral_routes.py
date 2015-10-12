90a06b57afc8270bfe5dbe154876f3ec7ce12c0b
fredkingham <fredkingham@gmail.com>
2015-10-12 17:09:27 +0100

    additional tests to cover the new additional model code

--- referral/tests/test_referral_routes.py
+++ referral/tests/test_referral_routes.py
@@ -4,26 +4,38 @@ unittests for referral.routes
 from opal.core.test import OpalTestCase
 
 from referral.routes import ReferralRoute
+from opal.tests.models import Colour
+
 
 class TestRoute(ReferralRoute):
-    name            = 'Test Route'
-    description     = 'This is a Route we use for unittests'
-    target_teams    = ['test']
+    name = 'Test Route'
+    description = 'This is a Route we use for unittests'
+    target_teams = ['test']
     target_category = ['testing']
-    success_link    = '/awesome/fun/times/'
+    success_link = '/awesome/fun/times/'
+    additional_models = [Colour]
+
 
 class ReferralRouteTestCase(OpalTestCase):
 
     def test_to_dict(self):
         expected = {
-            'name'            : 'Test Route',
-            'description'     : 'This is a Route we use for unittests',
-            'slug'            : 'test_route',
-            'success_link'    : '/awesome/fun/times/',
-            'verb'            : 'Refer',
-            'past_verb'       : 'Referred',
+            'additional_models': [("Colour", [
+                {'lookup_list': None, 'type': 'date_time', 'name': 'created', 'title': 'Created'},
+                {'lookup_list': None, 'type': 'date_time', 'name': 'updated', 'title': 'Updated'},
+                {'lookup_list': None, 'type': 'forei', 'name': u'created_by_id', 'title': u'Created By Id'},
+                {'lookup_list': None, 'type': 'forei', 'name': u'updated_by_id', 'title': u'Updated By Id'},
+                {'lookup_list': None, 'type': 'token', 'name': 'consistency_token', 'title': 'Consistency Token'},
+                {'lookup_list': None, 'type': 'string', 'name': 'name', 'title': 'Name'}
+            ])],
+            'name': 'Test Route',
+            'description': 'This is a Route we use for unittests',
+            'slug': 'test_route',
+            'success_link': '/awesome/fun/times/',
+            'verb': 'Refer',
+            'past_verb': 'Referred',
             'progressive_verb': 'Referring',
-            'page_title'      : None
+            'page_title': None
         }
-        self.assertEqual(expected, TestRoute.to_dict())
 
+        self.assertEqual(expected, TestRoute.to_dict())
