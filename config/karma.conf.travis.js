9ecfae3932a85f84a46f1831e1139f61ade9abc6
fredkingham <fredkingham@gmail.com>
2016-03-31 12:39:22 +0100

    removed karma developer

--- config/karma.conf.travis.js
+++ config/karma.conf.travis.js
@@ -1,67 +1,77 @@
 module.exports = function(config){
-    config.set({
-        frameworks: ['jasmine'],
-        browsers: ['Firefox'],
-        basePath:  '/home/travis/virtualenv/python2.7/src/opal/opal/static/js',
+  var browsers, basePath;
 
-        files: [
-          //JASMINE,
-          //JASMINE_ADAPTER,
-          'angular-1.2.20/angular.js',
-          'angular-1.2.20/angular-route.js',
-          'angular-1.2.20/angular-resource.js',
-          'angular-1.2.20/angular-cookies.js',
-          'angular-1.2.20/angular-mocks.js',
+  if(process.env.TRAVIS){
+      browsers = ["Firefox"];
+      basePath = '/home/travis/virtualenv/python2.7/src/opal/opal/static/js';
+  }
+  else{
+      browsers = ['PhantomJS'];
+      basePath = '../../opal/opal/static/js';
+  }
 
-          'angular-ui-utils-0.1.0/ui-utils.js',
-          'angular-ui-bootstrap-0.10.0/ui-bootstrap-tpls.js',
-          'angular-strap-2.3.1/angular-strap.js',
-          'angular-strap-2.3.1/modules/compiler.js',
-          'angular-strap-2.3.1/modules/tooltip.js',
-          'angular-strap-2.3.1/modules/tooltip.tpl.js',
-          'angular-strap-2.3.1/modules/dimensions.js',
-          'angular-strap-2.3.1/modules/parse-options.js',
-          'angular-strap-2.3.1/modules/date-parser.js',
-          'angular-strap-2.3.1/modules/datepicker.js',
-          'angular-strap-2.3.1/modules/datepicker.tpl.js',
-          'angular-strap-2.3.1/modules/timepicker.js',
-          'angular-strap-2.3.1/modules/timepicker.tpl.js',
-          'angular-strap-2.3.1/modules/typeahead.js',
-          'angular-strap-2.3.1/modules/typeahead.tpl.js',
-          "angulartics-0.17.2/angulartics.min.js",
-          "angulartics-0.17.2/angulartics-ga.min.js",
-          'ngprogress-lite/ngprogress-lite.js',
-          'jquery-1.11.3/jquery-1.11.3.js',
-          'utils/underscore.js',
-          'utils/moment.js',
-          'bower_components/angular-growl-v2/build/angular-growl.js',
-          'bower_components/ment.io/dist/mentio.js',
-          'bower_components/ment.io/dist/templates.js',
-          'bower_components/angular-ui-select/dist/select.js',
-          'opal/utils.js',
-          'opal/directives.js',
-          'opal/filters.js',
-          'opal/services_module.js',
-          'opal/services/*.js',
-          'opal/services/flow.js',
-          'opal/controllers_module.js',
-          'opal/controllers/*.js',
-          'opal/app.js',
-          '../../core/search/static/js/search/controllers/*',
-          '../../core/search/static/js/search/services/*',
+  config.set({
+      frameworks: ['jasmine'],
+      browsers: browsers,
+      basePath:  basePath,
+      files: [
+        //JASMINE,
+        //JASMINE_ADAPTER,
+        'angular-1.2.20/angular.js',
+        'angular-1.2.20/angular-route.js',
+        'angular-1.2.20/angular-resource.js',
+        'angular-1.2.20/angular-cookies.js',
+        'angular-1.2.20/angular-mocks.js',
 
-            // '../../../../elcid/elcid/assets/js/elcid/*.js',
-            __dirname+'/../referral/static/js/referral/*.js',
-            __dirname+'/../referral/static/js/referral/controllers/*.js',
-            __dirname+'/../referral/static/js/test/*.js',
+        'angular-ui-utils-0.1.0/ui-utils.js',
+        'angular-ui-bootstrap-0.10.0/ui-bootstrap-tpls.js',
+        'angular-strap-2.3.1/angular-strap.js',
+        'angular-strap-2.3.1/modules/compiler.js',
+        'angular-strap-2.3.1/modules/tooltip.js',
+        'angular-strap-2.3.1/modules/tooltip.tpl.js',
+        'angular-strap-2.3.1/modules/dimensions.js',
+        'angular-strap-2.3.1/modules/parse-options.js',
+        'angular-strap-2.3.1/modules/date-parser.js',
+        'angular-strap-2.3.1/modules/datepicker.js',
+        'angular-strap-2.3.1/modules/datepicker.tpl.js',
+        'angular-strap-2.3.1/modules/timepicker.js',
+        'angular-strap-2.3.1/modules/timepicker.tpl.js',
+        'angular-strap-2.3.1/modules/typeahead.js',
+        'angular-strap-2.3.1/modules/typeahead.tpl.js',
+        "angulartics-0.17.2/angulartics.min.js",
+        "angulartics-0.17.2/angulartics-ga.min.js",
+        'ngprogress-lite/ngprogress-lite.js',
+        'jquery-1.11.3/jquery-1.11.3.js',
+        'utils/underscore.js',
+        'utils/moment.js',
+        'bower_components/angular-growl-v2/build/angular-growl.js',
+        'bower_components/ment.io/dist/mentio.js',
+        'bower_components/ment.io/dist/templates.js',
+        'bower_components/angular-ui-select/dist/select.js',
+        'opal/utils.js',
+        'opal/directives.js',
+        'opal/filters.js',
+        'opal/services_module.js',
+        'opal/services/*.js',
+        'opal/services/flow.js',
+        'opal/controllers_module.js',
+        'opal/controllers/*.js',
+        'opal/app.js',
+        '../../core/search/static/js/search/controllers/*',
+        '../../core/search/static/js/search/services/*',
 
-        ],
+          // '../../../../elcid/elcid/assets/js/elcid/*.js',
+          __dirname+'/../referral/static/js/referral/*.js',
+          __dirname+'/../referral/static/js/referral/controllers/*.js',
+          __dirname+'/../referral/static/js/test/*.js',
 
-        // Stolen from http://oligofren.wordpress.com/2014/05/27/running-karma-tests-on-browserstack/
-        browserDisconnectTimeout : 10000, // default 2000
-        browserDisconnectTolerance : 1, // default 0
-        browserNoActivityTimeout : 4*60*1000, //default 10000
-        captureTimeout : 4*60*1000, //default 60000
+      ],
+
+      // Stolen from http://oligofren.wordpress.com/2014/05/27/running-karma-tests-on-browserstack/
+      browserDisconnectTimeout : 10000, // default 2000
+      browserDisconnectTolerance : 1, // default 0
+      browserNoActivityTimeout : 4*60*1000, //default 10000
+      captureTimeout : 4*60*1000, //default 60000
 
 
     })
