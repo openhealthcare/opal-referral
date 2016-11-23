3da986843a65b6668468886fc46af09480335013
fredkingham <fredkingham@gmail.com>
2016-11-23 17:20:35 +0000

    update to import the default karma config

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,48 +1,24 @@
 module.exports = function(config){
-    var browsers, basePath, coverageReporter;
-    var preprocessors = {};
-    preprocessors[__dirname+'/../referral/static/js/referral/*.js'] = 'coverage';
-    preprocessors[__dirname+'/../referral/static/js/referral/controllers/*.js'] = 'coverage';
-    preprocessors[__dirname+'/../referral/static/js/test/*.js'] = 'coverage';
-    var additionalDependencies;
+  var opalPath;
+  if(process.env.TRAVIS){
+    python_version = process.env.TRAVIS_PYTHON_VERSION;
+    opalPath = '/home/travis/virtualenv/python' + python_version + '/src/opal';
+  }
+  else{
+    opalPath = '../../opal';
+  }
+  var karmaDefaults = require(opalPath + '/config/karma_defaults.js');
+  var karmaDir = __dirname;
+  var coverageFiles = [
+    __dirname + '/../referral/static/js/referral/*.js',
+    __dirname + '/../referral/static/js/referral/controllers/*.js'
+  ];
+  var includedFiles = [
+      __dirname + '/../referral/static/js/referral/*.js',
+      __dirname + '/../referral/static/js/referral/controllers/*.js',
+      __dirname + '/../referral/static/js/test/*.js',
+  ];
 
-
-    if(process.env.TRAVIS){
-        browsers = ["Firefox"];
-        basePath = '/home/travis/virtualenv/python2.7/src/opal/opal/static/js';
-        coverageReporter = {
-            type: 'lcovonly', // lcov or lcovonly are required for generating lcov.info files
-            dir: __dirname + '/../coverage/',
-        };
-        additionalDependencies = require('/home/travis/virtualenv/python2.7/src/opal/config/karma_dependencies.js');
-    }
-    else{
-        browsers = ['PhantomJS'];
-        basePath = '../../opal/opal/static/js';
-        coverageReporter = {
-            type : 'html',
-            dir : '../../../htmlcov/js/'
-        };
-        additionalDependencies = require('../../opal/config/karma_dependencies.js');
-    }
-
-    config.set({
-        frameworks: ['jasmine'],
-        browsers: browsers,
-        basePath:  basePath,
-        files: additionalDependencies().concat([
-          __dirname+'/../referral/static/js/referral/*.js',
-          __dirname+'/../referral/static/js/referral/controllers/*.js',
-          __dirname+'/../referral/static/js/test/*.js',
-        ]),
-
-        // Stolen from http://oligofren.wordpress.com/2014/05/27/running-karma-tests-on-browserstack/
-        browserDisconnectTimeout : 10000, // default 2000
-        browserDisconnectTolerance : 1, // default 0
-        browserNoActivityTimeout : 4*60*1000, //default 10000
-        captureTimeout : 4*60*1000, //default 60000
-        preprocessors: preprocessors,
-        reporters: ['progress', 'coverage'],
-        coverageReporter: coverageReporter
-    })
-}
+  var defaultConfig = karmaDefaults(karmaDir, coverageFiles, includedFiles);
+  config.set(defaultConfig);
+};
