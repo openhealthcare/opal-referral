60d705047ed7485756737c89da83022106e9395a
David Miller <david@deadpansincerity.com>
2017-02-05 16:05:27 +0000

    Update to new plugin location

--- referral/__init__.py
+++ referral/__init__.py
@@ -1,5 +1,5 @@
 """
-Plugin definition
+Package definition
 """
 from django.conf import settings
 from opal.core import plugins
@@ -7,33 +7,3 @@ from opal.core import plugins
 from referral import api
 from referral.urls import urlpatterns
 from referral.routes import ReferralRoute
-
-menuitems = []
-DISPLAY_MENU = getattr(settings, 'REFERRAL_MENU_ITEM', True)
-if DISPLAY_MENU:
-    menuitems = [
-        dict(
-            href='/referrals/#/', display='Referrals', icon='fa fa-mail-forward',
-            activepattern='/referrals', index=2)
-    ]
-
-
-class ReferralPortalPlugin(plugins.OpalPlugin):
-    """
-    Main entrypoint to expose this plugin to the host
-    OPAL instance.
-    """
-    urls = urlpatterns
-    javascripts = {
-        'opal.referral': [
-            'js/referral/app.js',
-            'js/referral/services/referral_route_loader.js',
-            'js/referral/controllers/list.js',
-            'js/referral/controllers/referral.js',
-            # 'js/referral/controllers/episode_detail.js',
-        ]
-    }
-    menuitems = menuitems
-    apis = api.viewsets()
-
-plugins.register(ReferralPortalPlugin)
